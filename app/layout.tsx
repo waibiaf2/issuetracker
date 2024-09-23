5de37b7c0512ac5d2674df63f88598f633622328
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-23 19:18:24 +0300

    Initial configuration changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "@/app/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <NavBar/>
+      <main>
         {children}
+      </main>
       </body>
     </html>
   );
