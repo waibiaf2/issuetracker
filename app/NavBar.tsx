172cc6e75fee3e66acffd1dc5784363dec150648
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-23 20:05:17 +0300

    Styles active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import React from 'react';
 import Link from "next/link";
 import {FaBug} from "react-icons/fa";
+import {usePathname} from "next/navigation";
+import classnames from "classnames";
+
 
 const NavBar = () => {
+    const currentPath = usePathname();
+
     const links = [
         {label: 'Dashboard', href: '/'},
         {label: 'Issues', href: '/issues'}
@@ -18,11 +25,11 @@ const NavBar = () => {
                         <Link
                             key={link.label}
                             href={link.href}
-                            className="
-                                    text-zinc-500
-                                    hover:text-zinc-800
-                                    transition-colors
-                                    "
+                            className={classnames({
+                                'text-zinc-900': link.href === currentPath,
+                                'text-zinc-500': link.href !== currentPath,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}
                         >
                             {link.label}
                         </Link>
