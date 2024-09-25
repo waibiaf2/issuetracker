dd024d772cc575dbf09905bc7640cd1a314c61c0
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 17:13:14 +0300

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,11 +3,11 @@
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import {useForm, Controller} from "react-hook-form";
+import {useRouter} from "next/navigation";
 
 import {Button, TextField} from '@radix-ui/themes'
 import {HiMiniMagnifyingGlass} from "react-icons/hi2";
 import SimpleMDE from "react-simplemde-editor";
-import {useRouter} from "next/navigation";
 
 interface IssueForm {
     title: string;
@@ -17,25 +17,22 @@ interface IssueForm {
     updatedAt?: string;
 }
 
-
-
 const NewIssuePage = () => {
     const {register, handleSubmit, control} = useForm<IssueForm>();
     const router = useRouter();
 
-    const submitData = async (data: IssueForm) => {
-       await  axios.post('/api/issues', data)
-            .then((response) => {
-                console.log('Data submitted successfully.',response);
-                router.push('/issues');
-            }).catch((error) => {
-                console.log(error)
-        })
-    }
-
     return (
         <form
-            onSubmit={handleSubmit((data) => submitData(data))}
+            onSubmit = {
+                handleSubmit( async (data) => {
+                    try {
+                        await axios.post('/api/issues', data)
+                        router.push('/issues');
+                    } catch (err) {
+                        console.log(err);
+                    }
+                })
+            }
             className="max-w-xl space-y-4"
         >
             <TextField.Root {...register("title", {required: true})} >
@@ -54,6 +51,7 @@ const NewIssuePage = () => {
                 }
             />
             <Button
+                type="submit"
                 size={"3"}
                 className="hover:cursor-pointer"
             >
