52e3dc1d33ed5951f43e2d171bea50a3bba2695e
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 14:33:35 +0300

    add form submission functionality for the new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import {Button, TextArea, TextField} from '@radix-ui/themes'
+'use client'
+import {Button, TextField} from '@radix-ui/themes'
 import {HiMiniMagnifyingGlass} from "react-icons/hi2";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import {useForm, Controller} from "react-hook-form";
+
+interface IssueForm {
+    title: string;
+    description: string;
+}
+
 const NewIssuePage = () => {
+    const {register, handleSubmit, control} = useForm<IssueForm>();
+
     return (
-        <div className="max-w-xl space-y-3">
-           <TextField.Root>
-               <TextField.Slot>
-                   <HiMiniMagnifyingGlass />
-               </TextField.Slot>
-           </TextField.Root>
-            <TextArea placeholder="Description...." />
-            <Button size={"3"} className="hover:cursor-pointer">Submit New Issue</Button>
-        </div>
+        <form
+            onSubmit={handleSubmit((data) => console.log(data))}
+            className="max-w-xl space-y-4"
+        >
+            <TextField.Root {...register("title", {required: true})} >
+                <TextField.Slot>
+                    <HiMiniMagnifyingGlass/>
+                </TextField.Slot>
+            </TextField.Root>
+            <Controller
+                name="description"
+                control={control}
+                render={({field}) =>
+                    <SimpleMDE
+                        placeholder="Description"
+                        {...field}
+                    />
+                }
+            />
+            <Button
+                size={"3"}
+                className="hover:cursor-pointer"
+            >
+                Submit New Issue
+            </Button>
+        </form>
     );
 };
 
