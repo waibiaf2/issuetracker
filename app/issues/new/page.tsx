ec2b3fa8a3ab304a6e0ea8412be27a72a8c58c66
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 23:17:00 +0300

    implement client side validation...

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,25 +4,24 @@ import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import {Controller, useForm} from "react-hook-form";
 import {useRouter} from "next/navigation";
-
-import {Button, TextField} from '@radix-ui/themes'
+import {zodResolver} from "@hookform/resolvers/zod";
 import SimpleMDE from "react-simplemde-editor";
+import {z} from "zod";
 
-interface IssueForm {
-    title: string;
-    description: string;
-    status?: string;
-    createdAt?: string;
-    updatedAt?: string;
-}
+import {Button, TextField, Text} from '@radix-ui/themes'
+import {createIssueSchema} from "@/app/validationSchemas";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-    const {register, handleSubmit, control} = useForm<IssueForm>();
+    const {register, handleSubmit, control, formState: {errors}} = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    });
     const router = useRouter();
 
-    const handSubmitHandler = async (data: IssueForm) => {
+    const submitHandler = async (data: IssueForm) => {
         try {
-            await axios.post('/api/issues', data)
+            await axios.post('/api/issues', data);
             router.push('/issues');
         } catch (err) {
             console.log(err);
@@ -31,14 +30,22 @@ const NewIssuePage = () => {
 
     return (
         <form
-            onSubmit = {
-                handleSubmit((data) => handSubmitHandler(data) )
+            onSubmit={
+                handleSubmit((data) => submitHandler(data))
             }
             className="max-w-xl space-y-4"
         >
             <TextField.Root
                 {...register("title", {required: true})}
             />
+
+            {
+                errors.title &&
+                <Text color="red" as="p">
+                    {errors.title.message}
+                </Text>
+            }
+
             <Controller
                 name="description"
                 control={control}
@@ -49,6 +56,15 @@ const NewIssuePage = () => {
                     />
                 }
             />
+
+            {
+                errors.description &&
+                <Text color="red" as="p">
+                    {errors.description.message}
+                </Text>
+
+            }
+
             <Button
                 type="submit"
                 size={"3"}
