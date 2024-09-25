06dbc84ed303a2dea11de3dd35403188da49801a
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-09-25 15:45:58 +0300

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,13 @@
 'use client'
+
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import {useForm, Controller} from "react-hook-form";
+
 import {Button, TextField} from '@radix-ui/themes'
 import {HiMiniMagnifyingGlass} from "react-icons/hi2";
 import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import {useForm, Controller} from "react-hook-form";
+import {useRouter} from "next/navigation";
 
 interface IssueForm {
     title: string;
@@ -12,10 +16,21 @@ interface IssueForm {
 
 const NewIssuePage = () => {
     const {register, handleSubmit, control} = useForm<IssueForm>();
+    const router = useRouter();
+
+    const submitData = async (data) => {
+       await  axios.post('/api/issues', data)
+            .then((response) => {
+                console.log('Data submitted successfully.',response);
+                router.push('/issues');
+            }).catch((error) => {
+                console.log(error)
+        })
+    }
 
     return (
         <form
-            onSubmit={handleSubmit((data) => console.log(data))}
+            onSubmit={handleSubmit((data) => submitData(data))}
             className="max-w-xl space-y-4"
         >
             <TextField.Root {...register("title", {required: true})} >
